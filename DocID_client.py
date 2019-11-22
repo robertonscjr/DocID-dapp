bc4153f45223027af3299c7afbd54c2dae597702
Roberto Nascimento <robertonscjr@gmail.com>
2019-11-22 21:01:17 +0000

    Send registerIdentity transaction to DocID smart contract

--- DocID_client.py
+++ DocID_client.py
@@ -2,39 +2,56 @@ from web3 import Web3, HTTPProvider
 import json
 
 
-eth_uri = "127.0.0.1:8545"
+def sign_and_send_transaction(unsigned_tx, priv_key, web3_client):
+    """
+        the signing could be done in another application that will be run on
+        SGX to enhance security
+    """
+    try:
+        signed_tx = web3_client.eth.account.signTransaction(unsigned_tx,
+                                                            priv_key)
+
+        tx_hash = web3_client.eth.sendRawTransaction(signed_tx.rawTransaction)
+        return tx_hash
+
+    except:
+        raise
+
+
+address = "0x5EFd3143AaaeC686c2e27F7d3AfBD42d1f44c54C"
+private_key = "0x59edc805b16cc8c2d111a63c90337ab09868c010e0a96832ef422862c3a96553"
+
+eth_uri = "http://0.0.0.0:8545"
 contract_addr = "0x881ad5E0409f4982a74A0a0886ecF6B59782bfBa"
-abi = json.load(open("abi.json", "r").read())
+abi = json.loads(open("abi.json", "r").read())
 
-client = Web3(HTTPProvider(eth_uri))
-contract = client.eth.contract(address=config.contract_addr, abi=abi)
+web3_client = Web3(HTTPProvider(eth_uri))
+contract = web3_client.eth.contract(address=contract_addr, abi=abi)
 
 
-import pdb; pdb.set_trace()
-nonce = web3_client.eth.getTransactionCount(asset_provider_address)
+nonce = web3_client.eth.getTransactionCount(address)
 
-unsigned_tx = contract.functions.register(
-    name,
-    price,
-    asset_hash_bytes32,
-    manifest_hash_bytes32,
-    asset_sha256,
-    storage_type_uint,
-    file_type_uint,
-    [],
-    permissioned_applications_bf_bytes32,
-    bloom.BLOOM_FILTER_M,
-    bloom.BLOOM_FILTER_K
+first_name = "John"
+last_name = "Doe"
+date_of_birth = "01/01/1991"
+home_address = "Brasil"
+
+unsigned_tx = contract.functions.registerIdentity(
+    first_name,
+    last_name,
+    date_of_birth,
+    home_address
 )
+
 
 unsigned_tx = unsigned_tx.buildTransaction(
     {
         'nonce': nonce,
-        'from': asset_provider_address,
-        'value': value})
+        'from': address})
 
-tx_hash = ethereum.sign_and_send_transaction(unsigned_tx,
-                                             asset_provider_pk,
-                                             web3_client)
+tx_hash = sign_and_send_transaction(unsigned_tx,
+                                    private_key,
+                                    web3_client)
 
 
+import pdb; pdb.set_trace()
